8a34a9ffd8f8e4a47f7fde2b3dd51ce68c490a8d
Fenhl <fenhl@fenhl.net>
2026-02-24 02:38:54 +0000

    Enable mimalloc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 use {
     std::{
         cell::RefCell,
